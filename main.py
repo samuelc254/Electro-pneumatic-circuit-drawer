93502b42e1bfb16495386da78201e2bc0e075d47
Samuel Oliveira Costa <sam.oc2005@gmail.com>
2022-01-20 22:58:24 -0300

    teste com hiroku

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import os
 from flask import Flask, render_template
 
 app = Flask('Eletropnumatic circuit drawer')
 
 
+def main():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=port,
+        )
+
+
 @app.route('/')
-def homepage():
-    return render_template("homepage.html")
+def gerador():
+    return render_template('homepage.html')
 
 
-@app.route('/gerar/<sequencia>')
-def gerador(sequencia):
-    return 'arroz com ' + sequencia
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+    main()
