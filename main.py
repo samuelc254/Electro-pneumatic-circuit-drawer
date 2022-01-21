181ae3a9b430afade1a0f50dcc45951dcad4557f
Samuel Oliveira Costa <sam.oc2005@gmail.com>
2022-01-20 22:20:17 -0300

    inicio de testes api

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('Eletropnumatic circuit drawer')
 
+
 @app.route('/')
 def homepage():
-    return 'Hello World'
+    return render_template("homepage.html")
+
+
+@app.route('/gerar/<sequencia>')
+def gerador(sequencia):
+    return 'arroz com ' + sequencia
+
 
 app.run(debug=True)
