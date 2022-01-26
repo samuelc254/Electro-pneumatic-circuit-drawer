b58ae477df9c1339f712d5a7dd59f504369ead27
Samuel Oliveira Costa <sam.oc2005@gmail.com>
2022-01-25 23:06:09 -0300

    add path

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-import os
 import sys
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, HTMLResponse, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
+
+sys.path.append('./src')
 from drawer import Drawer
 import utils
 
